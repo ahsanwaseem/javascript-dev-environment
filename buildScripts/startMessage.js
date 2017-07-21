73e064334cf679c74f92a03ea757d0b2363ebb09
phpradio <allphpeverything@gmail.com>
2017-07-21 14:53:11 -0700

    added webpack etc

---
+++ buildScripts/startMessage.js
@@ -0,0 +1,4 @@
+import chalk from 'chalk';
+// this standard was implemented in ES6
+// chalk is a library that allows you to add color to the output in the console.
+console.log(chalk.green("Starting app in dev mode...."));
